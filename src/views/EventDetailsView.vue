9b8300cf03ef41c55967a7dea59149d340e3cab7
yxj0312 <yanxiajun@hotmail.com>
2023-02-22 22:18:48 +0100

    add v-if

--- src/views/EventDetailsView.vue
+++ src/views/EventDetailsView.vue
@@ -16,7 +16,7 @@ onMounted(() => {
 })
 </script>
 <template>
-  <div>
+  <div v-if="event">
     <h1>{{ event.title }}</h1>
     <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
     <p>{{ event.description }}</p>
