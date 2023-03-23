888318630b95d380417007dd380a491d675a43ff
yxj0312 <yanxiajun@hotmail.com>
2023-03-23 21:48:26 +0100

    import userEventStore

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -2,6 +2,7 @@
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
 import { ref, onMounted } from 'vue'
+import { useEventStore } from '@/services/EventService.js'
 
 const events = ref(null)
 
