03446166d504ee77ea63c408ceaec13c64c58835
yxj0312 <yanxiajun@hotmail.com>
2023-04-06 21:46:54 +0200

    add event actions

--- src/stores/EventStore.js
+++ src/stores/EventStore.js
@@ -12,10 +12,19 @@ export const useEventStore = defineStore('EventStore', {
     numberOfEvents: (state) => state.events.length,
   },
   actions: {
-    fetchEvent({ commit }, id) {
+    createEvent(event) {
+      return EventService.postEvent(event)
+        .then(() => {
+          this.events.push(event)
+        })
+        .catch((error) => {
+          throw error
+        })
+    },
+    fetchEvent(id) {
       return EventService.getEvent(id)
         .then((response) => {
-          commit('SET_EVENT', response.data)
+          this.event = response.data
         })
         .catch((error) => {
           throw error
